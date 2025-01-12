2ef3b640af3bc181db09c7286249900027a6cfc5
Jay Thakur <92983729+thakurjay1@users.noreply.github.com>
2025-01-12 14:37:30 +0530

    Import Package for @GetMapping Annotation

--- src/main/java/com/jenkins/controller/JenkinsController.java
+++ src/main/java/com/jenkins/controller/JenkinsController.java
@@ -3,6 +3,7 @@ package com.jenkins.controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api/jenkins")
